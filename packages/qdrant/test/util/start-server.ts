5e91c1159ca275af64dd069b6a3b47529f7b1fc4
chen <wszgrcy@gmail.com>
2026-05-07 13:02:47 +0800

    1

--- packages/qdrant/test/util/start-server.ts
+++ packages/qdrant/test/util/start-server.ts
@@ -3,7 +3,7 @@ import * as fs from 'fs';
 import { withResolvers } from './promise';
 import { spawn } from 'child_process';
 import { QD_RUNTIME_DIR } from './qdrant-runtime-dir';
-
+let dispose: () => Promise<any>;
 export async function startServer() {
   const cwd = process.cwd();
   await fs.promises.mkdir(QD_RUNTIME_DIR, { recursive: true });
@@ -21,10 +21,14 @@ export async function startServer() {
   console.log(QD_RUNTIME_DIR, exeFile);
 
   const start = withResolvers();
+  if (dispose) {
+    await dispose?.();
+  }
   const instance = spawn(exeFile, {
     cwd: QD_RUNTIME_DIR,
     env: { QDRANT__SERVICE__HTTP_PORT: '5432' },
   });
+
   instance.stdout?.on('data', (data) => {
     let message = data.toString();
     if (process.env.CI) {
@@ -35,21 +39,27 @@ export async function startServer() {
     }
   });
   await start.promise;
+  let disposed = false;
   // todo 删除
+  dispose = async () => {
+    if (disposed) {
+      return;
+    }
+    const end = withResolvers();
+    instance.on('close', () => {
+      fs.promises
+        .rm(QD_RUNTIME_DIR, { recursive: true, force: true })
+        .then(() => {
+          end.resolve();
+        });
+    });
+    instance.kill();
+    disposed = true;
+    return end.promise;
+  };
   return {
     // start$$: start.promise,
-    dispose: () => {
-      const end = withResolvers();
-      instance.on('close', () => {
-        fs.promises
-          .rm(QD_RUNTIME_DIR, { recursive: true, force: true })
-          .then(() => {
-            end.resolve();
-          });
-      });
-      instance.kill();
-      return end.promise;
-    },
+    dispose: dispose,
   };
 }
 export type ServerReturn = Awaited<ReturnType<typeof startServer>>;
